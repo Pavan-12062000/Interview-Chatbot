e3985a62bc3da30198768f107dc6d1ce597a43f7
Pavan12062000 <ppich075@uottawa.ca>
2024-11-16 19:49:55 -0500

    changed db connection file

--- chatbot_backend/src/dbconnection/dbconnection.js
+++ chatbot_backend/src/dbconnection/dbconnection.js
@@ -10,8 +10,8 @@ class getDbConnection {
         pool = new Pool({
           user: 'postgres',
           host: 'localhost', // usually 'localhost' if your database is hosted locally
-          database: 'Chatbot',
-          password: 'Sahithi@2002',
+          database: 'keep your database name',
+          password: 'keep your password',
           port: '5432', // usually 5432 for PostgreSQL
         });
 
